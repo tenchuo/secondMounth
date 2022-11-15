b83f476c8db3eca5bf79a31866942661469f4624
tenchuo <118255741+tenchuo@users.noreply.github.com>
2022-11-15 16:57:11 +0600

    dz 2

---
+++ 2-DZ2_Teniz_20-3/script.js
@@ -0,0 +1,22 @@
+var array = [];    
+array.push(0)
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+var i = 20
+ while (i <= 0) { 
+  console.log(i)
+  i-- 
+}
+
+
+var arr = prompt ('зеленый желтый красный');
+
+   if (arr === 'зеленый') {
+      alert('идти');
+   } else if (arr === 'желтый') {
+      alert('жди');
+   } else if (arr === 'красный') {
+      alert('стоп');
+  };
